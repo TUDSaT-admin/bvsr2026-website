9871c7b8cdbd55f54eb6f703dc9f0671c8029df8
Jaghan Vishwanathan A <jaghan2001@gmail.com>
2025-08-24 13:09:54 +0200

    Home Page and Navbar Updates

--- frontend/src/bvsr/home/home.component.ts
+++ frontend/src/bvsr/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 import { NavbarComponent } from "../navbar/navbar.component";
+import { MaterialModule } from '../../material/material.module';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, MaterialModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
