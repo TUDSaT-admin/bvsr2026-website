0533d5e9e7484f26f224747039903d34058d1f14
Jaghan Vishwanathan A <jaghan2001@gmail.com>
2025-08-31 20:47:18 +0200

    Sponser Form Functionality - Google Sheets

--- frontend/src/bvsr/sponsor-form-dialog/sponsor-form-dialog.component.ts
+++ frontend/src/bvsr/sponsor-form-dialog/sponsor-form-dialog.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../../material/material.module';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sponsor-form-dialog',
   standalone: true,
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, ReactiveFormsModule],
   templateUrl: './sponsor-form-dialog.component.html',
   styleUrls: ['./sponsor-form-dialog.component.css']
 })
-export class SponsorFormDialogComponent {}
+export class SponsorFormDialogComponent {
+  form: FormGroup;
+  submitting = false;
+  successMsg = '';
+  errorMsg = '';
+
+  private scriptURL =
+    'https://script.google.com/macros/s/AKfycbzg8tFMijrWWNMRzKnHNsWUaLvBuHDjWz12CI4s6qSUmqsxyZfm0N3owqKy6lWGsCHnLg/exec';
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      companyName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      sponsorTier: ['', Validators.required],
+      message: ['']
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid) return;
+    this.submitting = true;
+    const formData = new FormData();
+    Object.keys(this.form.value).forEach((key) => {
+      formData.append(key, this.form.value[key]);
+    });
+
+    fetch(this.scriptURL, { method: 'POST', body: formData })
+      .then(() => {
+        this.successMsg = '✅ Form submitted successfully!';
+        this.form.reset();
+        this.submitting = false;
+        setTimeout(() => (this.successMsg = ''), 5000);
+      })
+      .catch((err) => {
+        console.error('Error!', err);
+        this.errorMsg = '❌ Submission failed. Please try again.';
+        this.submitting = false;
+      });
+  }
+}
+
